77a55dcec698675f80e62f3ff944bd6a3818cb3f
Orion Teles de Mesquita <orionteles@gmail.com>
2025-05-17 23:15:40 -0300

    Create home page with navigation menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { PerfisComponent } from './perfis/perfis.component';
+import { HomeComponent } from './home/home.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'perfis', component: PerfisComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PerfisComponent
+    PerfisComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,5 @@
-import { Component } from '@angular/core';
-import { bootstrapApplication } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-@Component({
-  selector: 'app-root',
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-  `,
-})
-export class App {
-  name = 'Angular';
-}
-
-bootstrapApplication(App);
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
